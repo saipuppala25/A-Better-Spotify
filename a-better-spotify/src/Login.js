b02fbaa97a95ec101e5217ae0625c7914370dc9f
Kevin <kchen03@git.cs.vt.edu>
2024-02-21 10:54:56 -0500

    Tried changing login to directly link to spotify.

--- a-better-spotify/src/Login.js
+++ a-better-spotify/src/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Login() {
+
     return (
         <div className="App">
             <header className="App-header">
-                <a className="btn-spotify" href="/auth/login" >
-                    Login with Spotify 
+                <a className="btn-spotify" href="https://accounts.spotify.com/authorize/?" >
+                    Click here to login!
                 </a>
             </header>
         </div>
